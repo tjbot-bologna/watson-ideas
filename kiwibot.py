7049dbcc52c514fa87eb8a5a83114e37a6eac29d
Alessandro Norfo <sprintingkiwi@gmail.com>
2018-12-14 15:42:07 +0100

    .

--- kiwibot.py
+++ kiwibot.py
@@ -11,11 +11,16 @@ from telegram.error import NetworkError, Unauthorized
 import json
 from watson_developer_cloud import AssistantV1
 from watson_developer_cloud import VisualRecognitionV3
+from watson_developer_cloud import SpeechToTextV1
+from os.path import join, dirname
 
 
 # Define global variables
 update_id = None
-context = None
+info = {}
+info["context"] = None
+info["stt"] = False
+info["lang"] = "it"
 
 # Instance Watson resources objects
 assistant = AssistantV1(
@@ -26,6 +31,31 @@ assistant = AssistantV1(
 visual_recognition = VisualRecognitionV3(
     "2018-03-19",
     iam_apikey="UDDvweJSDR32NAZIfyxZ-dlvjIZ8dfRv75Z93xyjWGEX")
+
+
+# Watson assistant service function
+def converse(text):
+    response = assistant.message(
+        workspace_id="85e09b72-06f2-434d-9f2e-c2897ec4a611",
+        input={
+            "text": text
+        },
+        context=info["context"]).get_result()
+    print(json.dumps(response, indent=2))
+    # Update conversation context
+    info["context"] = response["context"]
+    return response
+
+
+# Visual recognition service function
+def vrec(pic_file):
+    # Send image to your Watson visual recognition resource
+    with open(pic_file, 'rb') as image:
+        classes = visual_recognition.classify(
+            image,
+            threshold='0.6').get_result()
+        print(json.dumps(classes, indent=2))
+        return classes
 
 
 # Simple echo function for tests
@@ -41,8 +71,8 @@ def echo(bot):
 
 
 # Main function to handle incoming messages
-def converse(bot):
-    global update_id, context
+def kiwi_answer(bot):
+    global update_id
     # Request updates after the last update_id
     for update in bot.get_updates(offset=update_id, timeout=10):
         update_id = update.update_id + 1
@@ -50,38 +80,47 @@ def converse(bot):
         if update.message:  # your bot can receive updates without messages
             # Reply to the message:
 
+            # Check if the user sent a voice note
+            if update.message.voice is not None:
+                print("Received a voice note! Speech to text in progress...")
+                voice_note = bot.get_file(update.message.voice.file_id)
+                voice_file = voice_note.download()
+
             # Check if the user sent a picture
             if len(update.message.photo) > 0:
+                print("Received a Picture! Visual recognition in progress...")
                 picture = bot.get_file(update.message.photo[-1].file_id)
-                pic_file = picture.download()
-
-                # Send image to your Watson visual recognition resource
-                with open(pic_file, 'rb') as image:
-                    classes = visual_recognition.classify(
-                        image,
-                        threshold='0.6').get_result()
-                    print(json.dumps(classes, indent=2))
-
-                    # Send Watson answer in Telegram chat
-                    update.message.reply_text(json.dumps(classes, indent=2))
+                pic_file = picture.download()                    
+                # Send Watson answer in Telegram chat
+                update.message.reply_text((json.dumps(vrec(pic_file), indent=2)))
         
             # Check if the user sent text
             if update.message.text is not None:
                 # Send text to watson assistant workspace (or skill) and get a response
-                response = assistant.message(
-                    workspace_id="85e09b72-06f2-434d-9f2e-c2897ec4a611",
-                    input={
-                        "text": update.message.text
-                    },
-                    context=context).get_result()
-                print(json.dumps(response, indent=2))
-
-                # Update conversation context
-                context = response["context"]
+                response = converse(update.message.text)
 
                 # Make the bot reply in the Telegram chat using the Watson response:
                 # This is for text answers, which might be multiple
                 for t in response["output"]["text"]:
+
+                    # Parse TJBot conversation command tags
+                    tag = None
+                    parsed_text = t.split("TJBOT_")
+                    elements = len(parsed_text)
+                    # Check if there are more than one tag comment per answer
+                    if elements > 2:
+                        update.message.reply_text("WARNING: Watson conversation can have only one tag command per answer")
+                        return
+                    elif elements > 1 and  elements < 3:
+                        tag = parsed_text[1]
+                        # Execute tag commands:
+                        if tag == "STT_START":
+                            info["stt"] = True
+                        elif tag == "STT_STOP":
+                            info["stt"] = False
+                        t = parsed_text[0]
+
+                    # Post text in Telegram chat
                     update.message.reply_text(t)
 
                 # This is for pictures link the response might contain
@@ -112,7 +151,7 @@ def main():
     while True:
         try:
             # echo(bot)
-            converse(bot)
+            kiwi_answer(bot)
         except NetworkError:
             sleep(1)
         except Unauthorized:

386639f502e716a57caff5be975e5578e697d428
Alessandro Norfo <sprintingkiwi@gmail.com>
2018-12-14 12:07:29 +0100

    added comments

--- kiwibot.py
+++ kiwibot.py
@@ -1,28 +1,24 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Simple Bot to reply to Telegram messages.
-This is built on the API wrapper, see echobot2.py to see the same example built
-on the telegram.ext bot framework.
-This program is dedicated to the public domain under the CC0 license.
-"""
+
+from time import sleep
+
+# Import Telegram libraries
 import logging
 import telegram
 from telegram.error import NetworkError, Unauthorized
-from time import sleep
 
+# Import Watson libraries
 import json
 from watson_developer_cloud import AssistantV1
 
-context = None
 
+update_id = None
+context = None
 assistant = AssistantV1(
     iam_apikey="esXISM3aWlgM5HORbmxtKnQnG6_I-fTM3wFa2rLHYBL9",
     ## url is optional, and defaults to the URL below. Use the correct URL for your region.
     url="https://gateway.watsonplatform.net/assistant/api",
     version="2018-07-10")
-
-
-update_id = None
 
 
 def echo(bot):
@@ -46,27 +42,36 @@ def converse(bot):
         if update.message:  # your bot can receive updates without messages
             # Reply to the message
 
+            # Send text to watson assistant workspace (or skill) and get a response
             response = assistant.message(
                 workspace_id="85e09b72-06f2-434d-9f2e-c2897ec4a611",
                 input={
                     "text": update.message.text
                 },
                 context=context).get_result()
+            print(json.dumps(response, indent=2))
 
+            # Update conversation context
             context = response["context"]
 
-            print(json.dumps(response, indent=2))
+            # Make the bot reply in the Telegram chat using the Watson response
+            # This is for text answers, which might be multiple
             for t in response["output"]["text"]:
-                # print(t + "\n")
                 update.message.reply_text(t)
+
+            # This is for pictures link the response might contain
             if (len(response["output"]["generic"]) > 1):
-                bot.send_photo(chat_id=update.message.chat_id, photo=response["output"]["generic"][1]["source"])
+                try:
+                    pic = response["output"]["generic"][1]["source"]
+                    bot.send_photo(chat_id=update.message.chat_id, photo=pic)
+                except Exception(e):
+                    print(e)
 
 
-
+# Run the bot.
 def main():
-    """Run the bot."""
     global update_id
+
     # Telegram Bot Authorization Token
     bot = telegram.Bot('286358309:AAG-NF68tQam6aN1PLCt0g1gm4kB5XS-Sic')
 
@@ -91,5 +96,5 @@ def main():
 
 
 if __name__ == '__main__':
-    print("kiwibot running at https://telegram.me/sprintingkiwibot")
+    print("Bot running at https://telegram.me/sprintingkiwibot")
     main()
